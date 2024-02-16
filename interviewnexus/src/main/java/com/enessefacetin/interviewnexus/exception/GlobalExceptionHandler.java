fe373a62e1a2f5f81ecb37af884edc804b4cb627
Enes Sefa Çetin <cetinen21@itu.edu.tr>
2024-02-16 22:04:44 +0300

    Global exception handler added

---
+++ interviewnexus/src/main/java/com/enessefacetin/interviewnexus/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.enessefacetin.interviewnexus.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<Object> handleStudentNotFoundException(EntityNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+}
